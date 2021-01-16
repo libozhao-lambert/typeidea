4d7d671818564202a74eb497bf7a587811263c92
libozhao-lambert <libozhao@aliyun.com>
2021-01-16 21:44:29 +0800

    提交Markdown功能代码

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+import mistune
 
 
 # 分类Model
@@ -76,6 +77,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name="标题")
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须MarkDown格式")
+    content_html = models.TextField(verbose_name="正文html代码", blank=True, editable=False)
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     category = models.ForeignKey(Category, verbose_name="分类")
     tag = models.ManyToManyField(Tag, verbose_name="标签")
@@ -111,6 +113,10 @@ class Post(models.Model):
         
         return post_list, category
     """
+
+    def save(self, *args, **kwargs):
+        self.content_html = mistune.markdown(self.content)
+        super().save(*args, **kwargs)
 
     @classmethod
     def latest_posts(cls):
